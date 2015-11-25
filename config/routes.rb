337c5062adca60e075cd0ec193f742b7c80a79aa
Amin Rezapour <aminrezapour@gmail.com>
2015-11-24 17:51:39 -0800

    A copy of post model, called Question, is created

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
+  resources :questions
+
   resources :posts
-  
+
   get 'about' => 'welcome#about'
 
   # Note the alternative synthax root({to: 'welcome#index'})
