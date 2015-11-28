a74a40c0a57077f9fa390c0eb321f0305ed55aa2
Amin Rezapour <aminrezapour@gmail.com>
2015-11-28 14:53:16 -0800

    Add user defined scope in post model to arbitrarily order posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,12 @@ class Post < ActiveRecord::Base
   has_many :comments, dependent: :destroy
 
   default_scope { order('created_at DESC')}
+  # This is equivalent to:
+  #     def self.ordered_by_title
+  #       order('title DESC')
+  #     end
+  scope :ordered_by_title, -> { order('title DESC') }
+  scope :ordered_by_reverse_created_at, -> { order('created_at ASC') }
 
   validates :title, length: { minimum: 5 }, presence: true
   validates :body, length: { minimum: 20 }, presence: true
