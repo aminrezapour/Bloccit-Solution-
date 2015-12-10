6a5a87625daf4ac7367cc5719241dbf9ba3164e8
Amin Rezapour <aminrezapour@gmail.com>
2015-12-10 13:50:46 -0800

    Add post endpoints for create, update and delete

--- config/routes.rb
+++ config/routes.rb
@@ -25,7 +25,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: [:index, :show, :create, :update]
-      resources :topics, except: [:edit, :new]
+      resources :topics, except: [:edit, :new] do
+        put :create_post
+      end
     end
   end
 
